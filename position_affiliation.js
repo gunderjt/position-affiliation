d8fac318c33ad23529afc70cc7cad5296ac72e63
Jeffrey Gunderson <jeffrey.gunderson@gmail.com>
2014-09-15 13:35:22 -0600

    Added js and modified module to load js

---
+++ position_affiliation.js
@@ -0,0 +1,15 @@
+//For reference https://www.drupal.org/node/756722#behaviors
+
+(function($) {
+	Drupal.behaviors.position_affiliation = {
+		attach: function (context, settings) {
+			$('.end_date_show', context).on('click', function(){
+				if( jQuery(this).is(':checked')) {
+					jQuery(this).parent().parent().find(".end_date").parent().show();
+				} else {
+					jQuery(this).parent().parent().find(".end_date").parent().hide();
+				}
+			});			
+		}
+	};
+}) (jQuery);
